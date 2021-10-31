f32a34e17c5bb78a87a8f89006cfaf4339b9a20e
porkostomus <porkostomus@mail.com>
2021-10-31 16:53:36 -0700

    hook up sci and clojure-mode

--- src/la/app.cljs
+++ src/la/app.cljs
@@ -1,10 +1,18 @@
 (ns la.app
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            [la.editor :as editor :refer [update-editor! !tri eval-all]]
+            [la.math :refer [pi]]))
+
+(defonce tri
+  (r/atom {:vertices ["A" "B" "C"]
+           :lines [nil nil 1]
+           :angles [nil (/ pi 2) (/ pi 6)]}))
 
 (defn app []
   [:div#app
-   [:h1 "Linear Algebra with Clojurescript"]])
+   [:h1 "Linear Algebra with Clojurescript"]
+   [editor/editor (str @tri) !tri {:eval? true}]])
 
 (defn render []
   (rdom/render [app]
---
+++ src/la/math.cljs
@@ -0,0 +1,48 @@
+(ns la.math)
+
+(def pi js/Math.PI)
+
+(defn sq [n]
+  (* n n))
+
+(defn sqrt [n]
+  (.sqrt js/Math n))
+
+(defn sin [rad]
+  (.sin js/Math rad))
+
+(defn cos [rad]
+  (.cos js/Math rad))
+
+(defn tan [rad]
+  (.tan js/Math rad))
+
+(defn asin [rad]
+  (.asin js/Math rad))
+
+(defn acos [rad]
+  (.acos js/Math rad))
+
+(defn atan [rad]
+  (.atan js/Math rad))
+
+(defn csc
+  "Returns the cosecant, the reciprocal of the sine,
+   the ratio of the hypotenuse to the side opposite 
+   a given angle in a right triangle."
+  [h o]
+  (/ h o))
+
+(defn sec
+  "Returns the secant, the reciprocal of the cosine,
+   the ratio of the hypotenuse to the side adjacent to 
+   a given angle in a right triangle."
+  [h a]
+  (/ h a))
+
+(defn cot
+  "Returns the cotangent, the reciprocal of the tangent,
+   the ratio of the adjacent side 
+   to the opposite side of a right triangle."
+  [a o]
+  (/ a o))
