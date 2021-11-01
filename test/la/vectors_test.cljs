f08f0e197737341d2ab894435dfe8043aef2a6c5
porkostomus <porkostomus@mail.com>
2021-11-01 10:08:13 -0700

    unit vectors

--- test/la/vectors_test.cljs
+++ test/la/vectors_test.cljs
@@ -24,6 +24,14 @@
   (is (= (vectors/endpoint (/ 1 2) -4 -6) [-2 -3]))
   (is (= (vectors/endpoint (/ 1 4) 8 4) [2 1])))
 
+;; https://www.khanacademy.org/math/linear-algebra/vectors-and-spaces/vectors/e/unit-vector?modal=1
+
+(deftest unit-vector-test
+  (is (= (vectors/unit-vector 6 6)
+         {:x 6, :y 6, :denom "\\sqrt{72}", :value 8.48528137423857}))
+  (is (= (vectors/unit-vector -8 5)
+         {:x -8, :y 5, :denom "\\sqrt{89}", :value 9.433981132056603})))
+
 (comment
   (run-tests)
   )
