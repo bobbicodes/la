18a8a14de35fd370a93c761957bfc709fc80f692
porkostomus <porkostomus@mail.com>
2021-11-01 10:08:00 -0700

    unit vectors

--- src/la/vectors.cljs
+++ src/la/vectors.cljs
@@ -1,5 +1,5 @@
 (ns la.vectors
-  (:require [la.math :refer [abs]]))
+  (:require [la.math :refer [abs sq sqrt]]))
 
 (defn scalar [multiplier {:keys [magnitude direction]}]
   {:magnitude (* (abs multiplier) magnitude)
@@ -9,6 +9,13 @@
 
 (defn endpoint [scalar x y]
   [(* scalar x) (* scalar y)])
+
+(defn unit-vector [x y]
+    {:x x
+     :y y
+     :denom (str "\\sqrt{" (+ (sq x) (sq y)) "}")
+     :value
+     (sqrt (+ (sq x) (sq y)))})
 
 (comment
   (scalar (- (/ 1 3))
@@ -21,4 +28,10 @@
   (endpoint (- (/ 1 5)) -25 10)
   (endpoint 0.5 -4 -6)
   (endpoint 0.25 8 4)
+  (sqrt (+ (sq 6) (sq 6)))
+  (unit-vector 6 6)
+    (unit-vector -8 5)
+  (unit-vector 5 -2)
+  (unit-vector 2 7)
+  (unit-vector 2 -8)
   )
